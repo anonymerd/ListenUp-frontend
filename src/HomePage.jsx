1d32fc7189146444d437553b38577fb6ca9ba937
anonymerd <rhtbisht738@gmail.com>
2021-06-16 02:41:30 +0530

    Connected Frontend with Backend

--- src/HomePage.jsx
+++ src/HomePage.jsx
@@ -2,37 +2,68 @@ import React, { Component } from 'react';
 
 import Navbar from './homepage-component/Navbar/Navbar';
 import Player from './homepage-component/Player/Player';
+import { GoogleLogin } from 'react-google-login';
 
 import './HomePage.css';
 
 import mainImage from './assets/images/img12.jpg';
 import googleIcon from './assets/icons/google-icon.svg';
 
+const axios = require('axios');
+
+const SERVER_ADDRESS = 'http://localhost:8000/';
+
 export default class HomePage extends Component {
   state = {
-    isSongPlaying: false,
+    currSong: new Audio(),
+    songName: 'Rap God',
+    songArtist: 'Eminem',
+    albumArt:
+      'https://jitselemmens.com/newsite/wp-content/uploads/2017/08/rap_godb.jpg',
+    streamAddress: '',
     currSongTime: 0,
     currSongDuration: 0,
   };
 
-  currSong = new Audio('http://localhost:8000/');
+  async componentDidMount() {
+    try {
+      const result = await axios.get(`${SERVER_ADDRESS}music`);
+      const musicResult = result.data;
+      console.log(musicResult);
 
-  componentDidMount() {
-    this.currSong.addEventListener('canplaythrough', () => {
       this.setState({
-        currSongDuration: this.currSong.duration,
+        currSong: new Audio(musicResult.streamAddress),
       });
-    });
-    this.currSong.ontimeupdate = () => {
-      this.setState({
-        currSongTime: this.currSong.currentTime,
+
+      this.state.currSong.addEventListener('canplaythrough', () => {
+        this.setState({
+          songName: musicResult.song,
+          songArtist: musicResult.artist,
+          albumArt: musicResult.thumbnail.url,
+          currSongDuration: this.state.currSong.duration,
+        });
       });
-    };
+      this.state.currSong.ontimeupdate = () => {
+        this.setState({
+          currSongTime: this.state.currSong.currentTime,
+        });
+      };
+    } catch (err) {
+      console.log(err);
+    }
   }
 
   toggleSong = () => {
-    if (this.currSong.paused) this.currSong.play();
-    else this.currSong.pause();
+    if (this.state.currSong.paused) this.state.currSong.play();
+    else this.state.currSong.pause();
+  };
+
+  loginSuccess = (res) => {
+    console.log(res);
+  };
+
+  loginFaliure = (res) => {
+    console.log(res);
   };
 
   render() {
@@ -47,14 +78,22 @@ export default class HomePage extends Component {
                 All your favourite artists at one spot.
               </div>
               <div className='signin-button'>
-                <button>
+                <GoogleLogin
+                  className='google-login-button'
+                  clientId='390511031158-234aa4gmc6oadsj6inuku9hi9f6ug8vq.apps.googleusercontent.com'
+                  onSuccess={this.loginSuccess}
+                  onFailure={this.loginFaliure}
+                  cookiePolicy={'single_host_origin'}
+                  isSignedIn={true}
+                  icon={false}
+                >
                   <img
                     src={googleIcon}
                     alt='Google Icon'
                     className='google-icon'
                   />
                   <span>Sign In With Google</span>
-                </button>
+                </GoogleLogin>
               </div>
             </section>
             <section className='main-section-right'>
@@ -63,14 +102,13 @@ export default class HomePage extends Component {
                   <div
                     className='track-icon'
                     style={{
-                      backgroundImage:
-                        'url(https://www.gannett-cdn.com/media/USATODAY/USATODAY/2013/08/27/1377616803000-XXX-D0-EMINEM-COVER-ALBUM-21-GRAPHIC.jpg)',
+                      backgroundImage: `url(${this.state.albumArt})`,
                     }}
                   ></div>
                   <div className='track-info-container'>
                     <span className='now-playing'>Now Playing</span>
-                    <span className='track-name'>The Real Slim Shady</span>
-                    <span className='artist-name'>Eminem</span>
+                    <span className='track-name'>{this.state.songName}</span>
+                    <span className='artist-name'>{this.state.songArtist}</span>
                   </div>
                 </div>
                 <div
